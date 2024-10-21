b7fbdb2096f842d3bd0e5ce741df63669f86ac58
matrixsakini <matrixsakini@gmail.com>
2024-10-21 18:04:54 +0000

    performance

---
+++ nextjs-webpack/dist/bundle.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./pages/index.js":
+/*!************************!*\
+  !*** ./pages/index.js ***!
+  \************************/
+/***/ (() => {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\n/home/student/mycode/nextjs-webpack/node_modules/babel-loader/lib/index.js:183\\n      externalDependencies?.forEach(dep => {\\n                           ^\\n\\nSyntaxError: Unexpected token '.'\\n    at wrapSafe (internal/modules/cjs/loader.js:915:16)\\n    at Module._compile (internal/modules/cjs/loader.js:963:27)\\n    at Object.Module._extensions..js (internal/modules/cjs/loader.js:1027:10)\\n    at Module.load (internal/modules/cjs/loader.js:863:32)\\n    at Function.Module._load (internal/modules/cjs/loader.js:708:14)\\n    at Module.require (internal/modules/cjs/loader.js:887:19)\\n    at require (internal/modules/cjs/helpers.js:85:18)\\n    at loadLoader (/home/student/mycode/nextjs-webpack/node_modules/loader-runner/lib/loadLoader.js:19:17)\\n    at iteratePitchingLoaders (/home/student/mycode/nextjs-webpack/node_modules/loader-runner/lib/LoaderRunner.js:182:2)\\n    at runLoaders (/home/student/mycode/nextjs-webpack/node_modules/loader-runner/lib/LoaderRunner.js:398:2)\");\n\n//# sourceURL=webpack://nextjs-webpack/./pages/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./pages/index.js"]();
+/******/ 	
+/******/ })()
+;
